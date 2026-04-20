6abb568ef6d019e47f9ab266c70daa19e1dcccdf
sophi <sofiatrigueros.2810@gmail.com>
2026-04-19 19:05:37 -0600

    actualizacion

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,3 +2,30 @@
 #include <string>
 #include <cctype>
 using namespace std;
+
+bool es_palindromo (string palabra) {
+    int inicio = 0;
+    int fin = palabra.lenght () -1;
+
+    while (inicio < fin){
+        if (tolower(palabra[inicio]) != tolower (palabra[fin])){
+            return false;
+        }
+        inicio++;
+        fin--;
+    }
+    return true;
+}
+
+int main(){
+    string palabra;
+
+    cin >> palabra;
+
+    if (es_palindromo(palabra)){
+        cout << "\"" << palabra << "\" es un palindromo." << endl;
+    } else {
+        cout << "\"" << palabra << "\" no es un palindromo." << endl;
+    }
+    return 0;
+}
