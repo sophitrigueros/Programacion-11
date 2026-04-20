e8223fcd18be0d981240c891e8b32cf909225e33
sophi <sofiatrigueros.2810@gmail.com>
2026-04-19 18:32:23 -0600

    actualizacion

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -10,7 +10,7 @@ string tipo_triangulo (int a, int b, int c){
     if (a == b && b == c){
         return "equilatero";
     } else if (a == b || a == c || b == c ){
-        return "isoceles";
+        return "isosceles";
     } else {
         return "escaleno";
     }
